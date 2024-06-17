9f602d47c98fef7cca7e494ad87969c6d310ba6f
Viktor Farcic <viktor@farcic.com>
2024-06-17 22:34:18 +0200

    Uninstall

---
+++ uninstall.sh
@@ -0,0 +1,20 @@
+brew uninstall font-fira-code-nerd-font
+
+brew uninstall starship
+
+brew uninstall thefuck
+
+brew uninstall font-fira-code
+
+brew uninstall eza
+
+brew uninstall fzf
+
+brew uninstall zoxide
+
+brew uninstall bat
+
+sudo rm -rf \
+    /usr/local/bin/devbox \
+    ~/.cache/devbox \
+    .local/share/devbox
