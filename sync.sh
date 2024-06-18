16445a50fd903f6a69efcf9b54a769c17cb99294
Viktor Farcic <viktor@farcic.com>
2024-06-18 18:16:12 +0200

    Sync

--- sync.sh
+++ sync.sh
@@ -9,4 +9,4 @@ stow .
 echo "## Follow the instructions at https://github.com/tonsky/FiraCode/wiki/VS-Code-Instructions to enable Fira Code in VS Code" \
     | gum format
 
-echo '## Execute `source .zshrc`.' | gum format
+echo '## Execute `source ~/.zshrc`.' | gum format
